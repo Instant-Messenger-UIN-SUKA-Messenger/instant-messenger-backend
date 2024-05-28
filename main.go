06bb25cca9110f6f007d0c5cb68ce3ad2d0d25a2
fellis-cp <60042724+fellis-cp@users.noreply.github.com>
2024-05-28 10:52:50 +0700

    Feat Weabsocket
    Add websocket

--- main.go
+++ main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"instant-messenger-backend/rabbitmq"
+	"context"
 	"instant-messenger-backend/database"
+	"instant-messenger-backend/rabbitmq"
 	"instant-messenger-backend/routes"
+	"instant-messenger-backend/websocket"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Initialize Gin router
 	r := gin.New()
+
+	// Connect to RabbitMQ
 	rabbitmq.ConnectToRabbitMQ()
+
+	// Connect to the database
 	database.ConnectDB()
+
+	// Setup routes
 	routes.SetupRouter(r)
-	r.Run("localhost:5000")
+
+	// Start WebSocket server
+	go websocket.StartWS()
+
+	// Start the HTTP server
+	srv := &http.Server{
+		Addr:    "localhost:5000",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to run HTTP server: %s", err)
+		}
+	}()
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	log.Println("Shutting down the server...")
+
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5)
+	defer cancel()
+
+	// Attempt to gracefully shutdown the server
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v", err)
+	}
+
+	log.Println("Server exited gracefully")
 }
