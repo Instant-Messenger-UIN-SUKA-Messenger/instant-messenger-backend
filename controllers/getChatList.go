fdeeaf2d34646ba6165533f60d799b994b8cfbe7
Nanda Arya Putra <nandaarya404@gmail.com>
2024-06-08 19:38:53 +0700

    chore: Set chat name to other participant's name in GetChatList controller if chat name empty (group chat)

--- controllers/getChatList.go
+++ controllers/getChatList.go
@@ -72,6 +72,34 @@ func GetChatList(c *gin.Context) {
 				})
 				return
 			}
+
+			// Check if chat has a name (because if private chat, chat name is empty)
+			if chat.Name == "" {
+				// Get Other Chat Participant userId
+				var otherParticipantID string
+				if len(chat.Participants) == 2 { // Assuming private chat has exactly 2 participants
+				  // Extract the other participant's userId based on whether it matches the current user's nim
+				  if chat.Participants[0] == nim {
+					otherParticipantID = chat.Participants[1]
+				  } else {
+					otherParticipantID = chat.Participants[0]
+				  }
+				}
+
+				// Fetch user details of the other participant based on their userId/nim
+				var otherParticipantUser models.User
+				err := userCollections.FindOne(context.TODO(), bson.M{"nim": otherParticipantID}).Decode(&otherParticipantUser)
+				if err != nil {
+					// Handle error fetching participant details
+					log.Printf("Error finding participant for chat %s: %v\n", chat.ChatID, err)
+					// You can decide how to handle this error (e.g., continue or return error)
+					continue
+				}
+
+				// Set chat name to other participant's name
+				chat.Name = otherParticipantUser.Name
+			}
+
 			chats = append(chats, chat)
 		}
 	}
