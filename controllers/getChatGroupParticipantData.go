8cf4f65730a740b3ab0dbc956fafc5a9943334c4
Nanda Arya Putra <nandaarya404@gmail.com>
2024-06-17 17:00:07 +0700

    Add controller to get participant data

--- controllers/getChatGroupParticipantData.go
+++ controllers/getChatGroupParticipantData.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"context"
+	"instant-messenger-backend/models"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetChatGroupParticipantData(c *gin.Context) {
@@ -20,55 +23,41 @@ func GetChatGroupParticipantData(c *gin.Context) {
 
 	log.Printf("Extracted participantsId: %s\n", participantsId)
 
-	// // Get Participant Data based on participantsId
-	// for _, participantId := range participantsId {
-	// 	// Find chat information based on chatID
-	// 	curChat, err := chatCollections.Find(context.TODO(), bson.M{"chatId": chatId})
-	// 	if err != nil {
-	// 		log.Printf("Error finding chat with ID %s: %v\n", chatId, err)
-	// 	}
+	type ParticipantData struct {
+		NIM  string `json:"userId"`
+		Name string `json:"name"`
+	}
 
-	// 	defer curChat.Close(context.TODO())
-	// 	for curChat.Next(context.TODO()) {
-	// 		var chat models.Chat
-	// 		err := curChat.Decode(&chat)
-	// 		log.Printf("%s", chat)
-	// 		if err != nil {
-	// 			c.JSON(http.StatusInternalServerError, gin.H{
-	// 				"error":   true,
-	// 				"message": err.Error(),
-	// 			})
-	// 			return
-	// 		}
+	var participantsData []ParticipantData
+	// Get Participant Data based on participantsId
+	for _, participantId := range participantsId {
+		// Find chat information based on participantId
+		cursorUser, err := userCollections.Find(context.TODO(), bson.M{"nim": participantId})
+		if err != nil {
+			log.Printf("Error finding user with nim %s: %v\n", participantId, err)
+		}
 
-	// 		// Check if chat has a name (because if private chat, chat name is empty)
-	// 		if chat.Name == "" {
-	// 			// Get Other Chat Participant userId
-	// 			var otherParticipantID string
-	// 			if len(chat.Participants) == 2 { // Assuming private chat has exactly 2 participants
-	// 				// Extract the other participant's userId based on whether it matches the current user's nim
-	// 				if chat.Participants[0] == nim {
-	// 					otherParticipantID = chat.Participants[1]
-	// 				} else {
-	// 					otherParticipantID = chat.Participants[0]
-	// 				}
-	// 			}
+		defer cursorUser.Close(context.TODO())
+		for cursorUser.Next(context.TODO()) {
+			var user models.User
+			err := cursorUser.Decode(&user)
+			log.Printf("%s", user)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error":   true,
+					"message": err.Error(),
+				})
+				return
+			}
 
-	// 			// Fetch user details of the other participant based on their userId/nim
-	// 			var otherParticipantUser models.User
-	// 			err := userCollections.FindOne(context.TODO(), bson.M{"nim": otherParticipantID}).Decode(&otherParticipantUser)
-	// 			if err != nil {
-	// 				// Handle error fetching participant details
-	// 				log.Printf("Error finding participant for chat %s: %v\n", chat.ChatID, err)
-	// 				// You can decide how to handle this error (e.g., continue or return error)
-	// 				continue
-	// 			}
+			var data = ParticipantData{user.NIM, user.Name}
 
-	// 			// Set chat name to other participant's name
-	// 			chat.Name = otherParticipantUser.Name
-	// 		}
+			participantsData = append(participantsData, data)
+		}
+	}
 
-	// 		chats = append(chats, chat)
-	// 	}
-	// }
+	c.JSON(http.StatusOK, gin.H{
+		"error":    false,
+		"participantData": participantsData,
+	})
 }
