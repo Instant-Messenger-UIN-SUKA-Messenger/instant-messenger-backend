29ac800236ba747ab6020ea45d71c4d9d2e8490a
Nanda Arya Putra <nandaarya404@gmail.com>
2024-06-25 11:00:08 +0700

    Add CORS Middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"instant-messenger-backend/database"
 	"instant-messenger-backend/rabbitmq"
 	"instant-messenger-backend/routes"
+	"instant-messenger-backend/utils"
 	"instant-messenger-backend/websocket"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,7 @@ import (
 
 func main() {
 	r := gin.New()
+	r.Use(utils.CORSMiddleware())
 	database.ConnectDB()
 	go websocket.InitGorillaWebsocket()
 	rabbitmq.ConnectToRabbitMQ()
